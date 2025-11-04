2c4fb2cc486e6334c695ef2f829e25cb3340ec96
BHOOMIKA740 <bhoomikae23cs@rnsit.ac.in>
2025-11-04 10:24:29 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,11 @@
 
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return "Hello! Your Capstone Project Web App is Running Successfully 🎉"
+
+if __name__ == '__main__':
+    app.run(debug=True)
